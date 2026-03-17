2b036f8e11f684edd79a31bbfa517f16d47e443f
Lee Kaburu <leekaburu@gmail.com>
2026-03-17 22:21:57 +0300

    Moved colors to general and added bcrypt handling

---
+++ app/helpers/general.py
@@ -0,0 +1,21 @@
+from colorama import init, Fore, Back, Style
+import bcrypt
+
+init(autoreset=True)
+
+class Colors:
+    RED = Style.BRIGHT + Fore.RED
+    BLUE = Style.BRIGHT + Fore.BLUE
+    YELLOW = Style.BRIGHT + Fore.YELLOW
+    GREEN = Style.BRIGHT + Fore.GREEN
+
+def hash_passwords(password:str)->bytes:
+    enc_pass = password.encode("utf-8")
+    salt = bcrypt.gensalt(rounds=12)
+    hashedpw = bcrypt.hashpw(enc_pass, salt)
+    return hashedpw
+
+def check_pass(password:str, hashed_pw:bytes)->bool:
+    enc_pass = password.encode("utf-8")
+    print(Colors.RED+f"{type(hashed_pw)}")
+    return bcrypt.checkpw(enc_pass, hashed_pw)
